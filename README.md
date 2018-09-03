a3dc55a47252ef12ddce10585848db880d0e61d9
Alejandro <ale64bit@gmail.com>
2018-09-03 11:03:42 -0400

    Add sample project sources

--- README.md
+++
@@ -1,2 +0,0 @@
-# repro6048
-Sample project for reproducing https://github.com/bazelbuild/bazel/issues/6048
---
+++ app/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <memory>
+
+#include "tools/cpp/runfiles/runfiles.h"
+
+using bazel::tools::cpp::runfiles::Runfiles;
+
+int main(int argc, char **argv) {
+  std::string error;
+  std::unique_ptr<Runfiles> runfiles(Runfiles::Create(argv[0], &error));
+  if (runfiles == nullptr) {
+    std::cerr << "failed to create Runfiles: " << error << '\n';
+    return -1;
+  }
+  std::string path = runfiles->Rlocation("data/test.txt");
+  std::cout << "path: " << path << '\n';
+
+  std::unique_ptr<std::FILE, decltype(&std::fclose)> fp(
+      std::fopen(path.c_str(), "r"), &std::fclose);
+  if (!fp) {
+    std::cerr << "file not found" << '\n';
+    return -1;
+  }
+  return 0;
+}
